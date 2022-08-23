478fd2f6c0be44b3b901ffd6d2a4e1d4ef43523a
FakeCoder01 <97719684+FakeCoder01@users.noreply.github.com>
2022-08-23 19:12:13 +0530

    Update settings.py

--- intellify/settings.py
+++ intellify/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z(*aob+x*8h9280aqk@d5d50b3huvo@3@1d84of4h!bh6bine%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
